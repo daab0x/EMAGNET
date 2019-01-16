6411d4d57c6afd935902fcad30f540997e87f960
wuseman <info@wuseman.com>
2019-01-16 22:20:51 +0100

    emagnet

--- .scripts/emagnet-bruteforce.sh
+++ .scripts/emagnet-bruteforce.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source /etc/emagnet.conf
+
 while :; do
 inotifywait -r $EMAGNETHOME/logs/emails-from-pastebin.txt &> /dev/null
 cat $EMAGNETHOME/logs/emails-from-pastebin.txt | grep gmail.com
--- .scripts/emagnet-help.sh
+++ .scripts/emagnet-help.sh
@@ -30,14 +30,14 @@ emagnet - is a tool that using the web browser lynx for all features
       -b BACKUPS  - Compress and backup emagnet's files
       -c CLONE    - Clone all repositories from a user on git
       -h HELP     - Print this help
-      -H DOWNLOAD - Download uploads on pastebin in background
-      -e EMAGNET  - Run emagnet until you find atleast 1 email address, then stop
-      -f FOREVER  - Run emagnet until you will kill emagnet.
-      -n NETCAT   - Upload anything to nr1.nu, eg: echo hello | emagnet -n
+      -H DOWNLOAD - Run emagnet in background, kill it with pkill -9 emagnet
+      -e EMAGNET  - Run emagnet until you find atleast 1 email address, then we stop (SAFE)
+      -f FOREVER  - Run emagnet until you will kill emagnet or cancel emagnet by (CTRL+C) (RISK FOR TEMP BAN)
+      -n NETCAT   - Upload anything to nr1.nu, eg: 'echo hello | emagnet -n'
       -m MIRROR   - Mirror any website and download all data that is possible
-      -p PING     - Is host up or down, if there is a website then we check with lynx otherwise for ipv4 we ping
       -s SPIDER   - This is a rare spider that collects ALL urls via lynx
       -v VERSION  - Print current version
+      -u UPORDOWN - Is host up or down, if there is a website then we check with lynx otherwise for ipv4 we ping
 
 EOF
 }
